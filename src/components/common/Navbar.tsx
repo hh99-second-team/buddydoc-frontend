9249413b056436e90b4c973f300dc5b23b06c4a1
곽민지 <mini0006@naver.com>
2024-02-06 19:05:10 +0900

    feat: Navbar 생성

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import * as Dialog from '@radix-ui/react-dialog';
+import styled from 'styled-components';
+import Modal from './Modal';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Layout>
+      Navbar
+      <Dialog.Root>
+        <Dialog.Trigger asChild>
+          <button className="Button violet">로그인</button>
+        </Dialog.Trigger>
+        <Dialog.Portal>
+          <Modal />
+        </Dialog.Portal>
+      </Dialog.Root>
+    </Layout>
+  );
 };
+
+const Layout = styled.div`
+  position: sticky;
+  top: 0;
+  width: 100%;
+  height: 57px;
+  padding: 0 2vw;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-shrink: 0;
+  border-bottom: 1px solid #d9d9d9;
+  background: #fff;
+`;
 
 export default Navbar;
