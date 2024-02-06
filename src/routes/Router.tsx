a795b5ac43c6c6a5beb7e5e85d29cc09b6b8c7b4
곽민지 <mini0006@naver.com>
2024-02-06 19:06:06 +0900

    feat: 모든 페이지에서 상단바 접근 가능

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from '../pages/Main';
 import MyPage from '../pages/MyPage';
 import PostDetail from '../pages/PostDetail';
+import Navbar from '../components/common/Navbar';
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/:id" element={<PostDetail />} />
