fefc65742a3c76cd95aa9ae52f5b9b0dba94377c
곽민지 <mini0006@naver.com>
2024-02-06 19:07:06 +0900

    Feat: Radix ui + styled components 를 이용한 모달 생성

---
+++ src/components/common/Modal.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import * as Dialog from '@radix-ui/react-dialog';
+import { Cross2Icon } from '@radix-ui/react-icons';
+import styled, { keyframes } from 'styled-components';
+
+const Modal = () => (
+  <>
+    <Overlay />
+    <Content>
+      <Title>TEST</Title>
+      <Description>test</Description>
+      <Close asChild>
+        <button className="IconButton" aria-label="Close">
+          <Cross2Icon />
+        </button>
+      </Close>
+    </Content>
+  </>
+);
+
+const overlayShow = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+const contentShow = keyframes`
+  from {
+    opacity: 0;
+    transform: translate(-50%, -48%) scale(0.96);
+  }
+  to {
+    opacity: 1;
+    transform: translate(-50%, -50%) scale(1);
+  }
+`;
+
+const Overlay = styled(Dialog.Overlay)`
+  background-color: rgba(0, 0, 0, 0.3);
+  position: fixed;
+  inset: 0;
+  animation: ${overlayShow} 150ms cubic-bezier(0.16, 1, 0.3, 1);
+`;
+
+const Content = styled(Dialog.Content)`
+  background-color: white;
+  border-radius: 6px;
+  box-shadow: hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 90vw;
+  max-width: 450px;
+  max-height: 85vh;
+  padding: 25px;
+  animation: ${contentShow} 150ms cubic-bezier(0.16, 1, 0.3, 1);
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+const Title = styled(Dialog.Title)`
+  margin: 0;
+  font-weight: 500;
+  color: var(--mauve-12);
+  font-size: 17px;
+`;
+
+const Description = styled(Dialog.Description)`
+  margin: 10px 0 20px;
+  color: var(--mauve-11);
+  font-size: 15px;
+  line-height: 1.5;
+`;
+
+const Close = styled(Dialog.Close)``;
+
+export default Modal;
