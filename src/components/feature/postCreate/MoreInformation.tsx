ce6afd43fabb54f1697ff724bf9c560dacf07f74
곽민지 <mini0006@naver.com>
2024-02-22 22:27:33 +0900

    Feat: 게시물 작성 페이지 개발 완료

--- src/components/feature/postCreate/MoreInformation.tsx
+++ src/components/feature/postCreate/MoreInformation.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styled from 'styled-components';
 import Input from '../../common/Input';
+import { Editor } from '@toast-ui/react-editor';
+import '@toast-ui/editor/dist/toastui-editor.css';
 
 interface inputInterface {
   type: string;
@@ -20,25 +22,43 @@ interface Props {
 }
 
 const MoreInformation = ({ inputVal, setInputVal }: Props) => {
+  const editorRef = useRef<Editor>(null);
+
   const onChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) => setInputVal({ ...inputVal, title: e.target.value });
+
+  const onChangeGetHTML = () => {
+    // 에디터에 입력된 내용을 HTML 태그 형태로 취득
+    const content = editorRef.current?.getInstance().getHTML();
+    // Body에 담기
+    setInputVal({ ...inputVal, content });
+  };
 
   return (
     <div>
       <Title>상세 정보 입력</Title>
       <Container>
-        <InputBox>
-          <p>제목</p>
-          <Input
-            type="text"
-            placeholder="모집 글 제목을 입력해주세요."
-            value={inputVal.title}
-            onChange={onChangeTitle}
-            isValid="none"
-          />
-        </InputBox>
-        <InputBox>
-          <p>본문 내용</p>
-        </InputBox>
+        <Input
+          type="text"
+          placeholder="모집 글 제목을 입력해주세요."
+          value={inputVal.title}
+          onChange={onChangeTitle}
+          isValid="none"
+        />
+        <Editor
+          toolbarItems={[
+            // 툴바 옵션 설정
+            ['heading', 'bold', 'italic', 'strike'],
+            ['hr', 'quote'],
+            ['ul', 'ol', 'task', 'indent', 'outdent'],
+            ['table', 'image', 'link'],
+            ['code', 'codeblock'],
+          ]}
+          height="500px" // 에디터 창 높이
+          initialEditType="markdown" // 기본 에디터 타입 (or wysiwyg)
+          previewStyle="vertical" // 미리보기 스타일 (or tab) (verttical은 양쪽이 나뉨)
+          ref={editorRef} // ref 참조
+          onChange={onChangeGetHTML} // onChange 이벤트
+        ></Editor>
       </Container>
     </div>
   );
@@ -53,17 +73,6 @@ const Container = styled.div`
   grid-template-columns: repeat(1, 1fr);
   row-gap: 40px;
   padding: 40px 0;
-`;
-
-const InputBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  column-gap: 10px;
-
-  & > p {
-    font-size: 20px;
-    margin-bottom: 12px;
-  }
 `;
 
 export default MoreInformation;
--- src/pages/PostCreate.tsx
+++ src/pages/PostCreate.tsx
@@ -4,6 +4,7 @@ import BasicInformation from '../components/feature/postCreate/BasicInformation'
 import MoreInformation from '../components/feature/postCreate/MoreInformation';
 import Button from '../components/common/Button';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 
 interface inputInterface {
   type: string;
@@ -18,6 +19,8 @@ interface inputInterface {
 }
 
 const PostCreate = () => {
+  const navigate = useNavigate();
+
   const [inputVal, setInputVal] = useState<inputInterface>({
     type: '',
     deadlineDate: new Date(),
@@ -30,15 +33,34 @@ const PostCreate = () => {
     content: '',
   });
 
+  const handleSubmit = () => {
+    if (
+      !inputVal.type ||
+      !inputVal.deadlineDate ||
+      !inputVal.startDate ||
+      !inputVal.period ||
+      !inputVal.tableOfOrganization ||
+      !inputVal.positons ||
+      !inputVal.selectedSkills ||
+      !inputVal.title ||
+      !inputVal.content
+    ) {
+      alert('모든 항목을 입력해주세요!');
+      return;
+    }
+    console.log(inputVal);
+    navigate('/');
+  };
+
   return (
     <CreateLayout>
       <BasicInformation inputVal={inputVal} setInputVal={setInputVal} />
       <MoreInformation inputVal={inputVal} setInputVal={setInputVal} />
       <ButtonSet>
-        <Button size="medium" color="black">
+        <Button size="medium" color="black" onClick={() => navigate('/')}>
           취소
         </Button>
-        <Button size="medium" color="primary">
+        <Button size="medium" color="primary" onClick={handleSubmit}>
           등록
         </Button>
       </ButtonSet>
