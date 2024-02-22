5383120678696386a497888de96264b4920bcae2
곽민지 <mini0006@naver.com>
2024-02-23 05:09:53 +0900

    Refactor: Main Layout 변경

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Banner from '../components/feature/Banner';
 import PostList from '../components/feature/PostList';
-import { Layout } from '../styles/GlobalStyles';
+import styled from 'styled-components';
 
 const Main = () => {
   return (
@@ -13,5 +13,9 @@ const Main = () => {
     </>
   );
 };
+
+const Layout = styled.div`
+  padding: 0 12vw 5vh 12vw;
+`;
 
 export default Main;
