92ab0c18d6c9c1b15891e5d7378729ca75502129
곽민지 <mini0006@naver.com>
2024-02-25 22:24:03 +0900

    Design: 메인 페이지 탭 반응형으로 변경

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,20 +2,114 @@ import React from 'react';
 import Banner from '../components/feature/Banner';
 import PostList from '../components/feature/PostList';
 import styled from 'styled-components';
+import * as Tabs from '@radix-ui/react-tabs';
+import { Layout } from '../styles/GlobalStyles';
+import Button from '../components/common/Button';
+import { useNavigate } from 'react-router-dom';
+import CoffeeChat from '../components/CoffeeChat';
 
 const Main = () => {
+  const navigate = useNavigate();
+
   return (
     <>
       <Banner />
       <Layout>
-        <PostList />
+        <TabsRoot defaultValue="all">
+          <TabHeader>
+            <TabsList aria-label="게시물 목록">
+              <TabsTrigger value="all">전체</TabsTrigger>
+              <TabsTrigger value="study">스터디</TabsTrigger>
+              <TabsTrigger value="project">프로젝트</TabsTrigger>
+              <TabsTrigger value="coffeeChat">커피챗</TabsTrigger>
+            </TabsList>
+            <Button size="large" color="primary" onClick={() => navigate('/create')}>
+              팀원 모집하기
+            </Button>
+          </TabHeader>
+          <TabsContent value="all">
+            <PostList />
+          </TabsContent>
+          <TabsContent value="study">
+            <PostList />
+          </TabsContent>
+          <TabsContent value="project">
+            <PostList />
+          </TabsContent>
+          <TabsContent value="coffeeChat">
+            <CoffeeChat />
+          </TabsContent>
+        </TabsRoot>
       </Layout>
     </>
   );
 };
 
-const Layout = styled.div`
-  padding: 0 12vw 5vh 12vw;
+const TabsRoot = styled(Tabs.Root)`
+  display: flex;
+  flex-direction: column;
+`;
+
+const TabHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  @media screen and (max-width: 768px) {
+    display: block;
+
+    & > button {
+      width: 100%;
+      margin-top: 1rem;
+    }
+  }
+`;
+
+const TabsList = styled(Tabs.List)`
+  display: flex;
+
+  @media screen and (max-width: 768px) {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+  }
+`;
+
+const TabsTrigger = styled(Tabs.Trigger)`
+  all: unset;
+  background-color: white;
+  height: 45px;
+  display: flex;
+  align-items: center;
+  justify-content: left;
+  user-select: none;
+  color: var(--grey03, #ced0d3);
+  font-family: Pretendard;
+  font-size: 1.5rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  padding-right: 30px;
+
+  &:hover {
+    color: darkgray;
+  }
+  &[data-state='active'] {
+    color: #000;
+  }
+
+  @media screen and (max-width: 768px) {
+    font-size: 1.3rem;
+    justify-content: center;
+  }
+`;
+
+const TabsContent = styled(Tabs.Content)`
+  flex-grow: 1;
+  padding: 20;
+  background-color: white;
+  border-bottom-left-radius: 6;
+  border-bottom-right-radius: 6;
+  outline: none;
 `;
 
 export default Main;
