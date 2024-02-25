39d71f504779de780be68dac417c08fffdf5f766
곽민지 <mini0006@naver.com>
2024-02-26 03:19:36 +0900

    Design: 모바일일 때 스크롤 버튼 좀 더 작게

--- src/components/common/TopButton.tsx
+++ src/components/common/TopButton.tsx
@@ -49,6 +49,11 @@ const GoToTopButton = styled.img`
     opacity: 1;
     visibility: visible;
   }
+
+  @media screen and (max-width: 768px) {
+    width: 4rem;
+    height: 4rem;
+  }
 `;
 
 export default TopButton;
